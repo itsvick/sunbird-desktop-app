5cfd485fac169207665f5a8c634ba6ceb55ee2cc
Nikunj Beladiya <33539271+Nik720@users.noreply.github.com>
2020-10-20 17:36:23 +0530

    Issue #SB-21067 fix: Language not changing back to english (#147)
    * Issue #SB-21067 fix: Language not changing back to english
    
    * Issue #SB-21067 fix: lint issue fix

--- src/client/src/app/modules/content-consumption/components/toc-page/toc-page.component.spec.ts
+++ src/client/src/app/modules/content-consumption/components/toc-page/toc-page.component.spec.ts
@@ -42,7 +42,8 @@ describe('TocPageComponent', () => {
         interactive: 'Interactive',
         docs: 'Docs'
       }
-    }
+    },
+    languageSelected$: of({})
   };
 
   beforeEach(async(() => {
--- src/client/src/app/modules/content-consumption/components/toc-page/toc-page.component.ts
+++ src/client/src/app/modules/content-consumption/components/toc-page/toc-page.component.ts
@@ -108,7 +108,17 @@ export class TocPageComponent implements OnInit, OnDestroy {
     .subscribe(x => {this.setPageExitTelemtry(); });
 
     this.contentService.contentFullScreenEvent.pipe(takeUntil(this.unsubscribe$)).subscribe(response => {
-     this.isFullScreenView = !this.isFullScreenView; }); }
+     this.isFullScreenView = !this.isFullScreenView; });
+
+    this.resourceService.languageSelected$.pipe(takeUntil(this.unsubscribe$)).subscribe(item => {
+      this.mimeTypeFilters = [
+        { text: this.resourceService.frmelmnts.btn.all, value: 'all' },
+        { text: this.resourceService.frmelmnts.btn.video, value: 'video' },
+        { text: this.resourceService.frmelmnts.btn.interactive, value: 'interactive' },
+        { text: this.resourceService.frmelmnts.btn.docs, value: 'docs' },
+      ];
+    });
+  }
 
   checkDownloadStatus(downloadListdata) {
     this.collectionData = this.playerService.updateDownloadStatus(downloadListdata, this.collectionData);
